65b5005e7557c897642a714429685e6f541fb51f
Nikita Tsvetkov <tsv1@fastmail.com>
2023-06-13 19:51:18 +0400

    bump version → 0.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro-httpx",
-    version="0.1.1",
+    version="0.1.2",
     description="Vedro + HTTPX",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
--- vedro_httpx/__init__.py
+++ vedro_httpx/__init__.py
@@ -3,6 +3,6 @@ from ._response import Response
 from ._sync_http_interface import SyncHTTPInterface
 from ._vedro_httpx import VedroHTTPX, VedroHTTPXPlugin
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ("VedroHTTPX", "VedroHTTPXPlugin",
            "SyncHTTPInterface", "AsyncHTTPInterface", "Response")
