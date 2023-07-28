95a6d9de828ec9005cfcdd889d12f6057ce4287d
Nikita Tsvetkov <tsv1@fastmail.com>
2023-07-28 20:46:26 +0400

    bump version → 0.3.0

--- vedro_httpx/__init__.py
+++ vedro_httpx/__init__.py
@@ -3,6 +3,6 @@ from ._response import Response
 from ._sync_http_interface import SyncHTTPInterface
 from ._vedro_httpx import VedroHTTPX, VedroHTTPXPlugin
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = ("VedroHTTPX", "VedroHTTPXPlugin",
            "SyncHTTPInterface", "AsyncHTTPInterface", "Response")
